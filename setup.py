9ae7a2ee56f0f85ca13d14f15ca3646e93b5ba6f
Jin Lee <leepc12@gmail.com>
2019-05-20 03:36:52 -0700

    added python3 requirements for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='caper',
-    version='v0.1.2.3',
+    version='v0.1.2.4',
+    python_requires='>3.4.1',
     scripts=['bin/caper', 'mysql/run_mysql_server_docker.sh',
              'mysql/run_mysql_server_singularity.sh'],
     author='Jin Lee',
