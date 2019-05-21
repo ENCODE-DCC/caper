23b4fda157926db7b7ac9256a4eb75d0f6e7e217
Jin Lee <leepc12@gmail.com>
2019-05-21 01:40:26 -0700

    added 'requests' to requirements

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
     ],
-    install_requires=['pyhocon']
+    install_requires=['pyhocon', 'requests']
 )
