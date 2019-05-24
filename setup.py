049ca18013f28d6eb7c8c9b458f6549aa5a8cc63
Jin Lee <leepc12@gmail.com>
2019-05-24 16:15:46 -0700

    remove long_description from setup.py, which causes weird rst format error

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='caper',
-    version='v0.1.8',
+    version='v0.1.9',
     python_requires='>3.4.1',
     scripts=['bin/caper', 'mysql/run_mysql_server_docker.sh',
              'mysql/run_mysql_server_singularity.sh'],
     author='Jin Lee',
     author_email='leepc12@gmail.com',
     description='Cromwell Assisted Pipeline ExecutoR',
-    long_description=long_description,
+    long_description='https://github.com/ENCODE-DCC/caper',  #long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ENCODE-DCC/caper',
     packages=setuptools.find_packages(exclude=['mysql*', 'docs']),
