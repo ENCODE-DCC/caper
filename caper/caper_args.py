2caf5b7d535e631918d6ffa94477b1cb024bf543
Jin Lee <leepc12@gmail.com>
2020-03-09 15:50:50 -0700

    ver: 0.6.5 -> 0.7.0

--- caper/caper_args.py
+++ caper/caper_args.py
@@ -17,7 +17,7 @@ from .caper_backend import BACKEND_ALIAS_LOCAL
 from .caper_backend import BACKEND_ALIAS_SHERLOCK, BACKEND_ALIAS_SCG
 
 
-__version__ = '0.6.5'
+__version__ = '0.7.0'
 
 DEFAULT_JAVA_HEAP_SERVER = '10G'
 DEFAULT_JAVA_HEAP_RUN = '3G'
