60001b1d35af507e7aed6a28ed810025dd147b91
Jin Lee <leepc12@gmail.com>
2020-04-01 10:29:01 -0700

    fix pip dep issue, ver up

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 import setuptools
-import caper
+import pkg_resources
+
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='caper',
-    version=caper.__version__,
-    python_requires='>3.4.1',
+    version=pkg_resources.get_distribution('caper').version,
+    python_requires='>=3.6',
     scripts=['bin/caper', 'bin/run_mysql_server_docker.sh',
              'bin/run_mysql_server_singularity.sh'],
     author='Jin Lee',
@@ -22,5 +23,5 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
     ],
-    install_requires=['pyhocon', 'requests', 'pyopenssl', 'autouri>=0.1.1']
+    install_requires=['pyhocon', 'requests', 'pyopenssl', 'autouri>=0.1.2']
 )
