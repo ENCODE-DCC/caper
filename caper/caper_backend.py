a87ab8bf0776028262ed5693f43c39448acc8a69
Jin Lee <leepc12@gmail.com>
2020-04-14 08:56:05 -0700

    fix indent

--- caper/caper_backend.py
+++ caper/caper_backend.py
@@ -106,12 +106,8 @@ class CaperBackendDatabase(UserDict):
             "url": "jdbc:hsqldb:file:{file};"
                 "shutdown=false;"
                 "hsqldb.tx=mvcc;"
-                "hsqldb.default_table_type=cached;"
-                "hsqldb.result_max_memory_rows=10000;"
-                "hsqldb.large_data=true;"
-                "hsqldb.applog=1;"
                 "hsqldb.lob_compressed=true;"
-                "hsqldb.script_format=3",
+                ,
             "connectionTimeout": 5000,
             "numThreads": 1
         }
