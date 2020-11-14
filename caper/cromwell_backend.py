21f7b79001450c47b047db8bffe247bee4b2367e
David Feinzeig <feinzeig@gmail.com>
2020-11-14 13:00:03 -0500

    per https://github.com/broadinstitute/cromwell/pull/5468 need to provide scriptBucketName to default-runtime-attributes for aws batch

--- caper/cromwell_backend.py
+++ caper/cromwell_backend.py
@@ -393,6 +393,7 @@ class CromwellBackendAWS(CromwellBackendBase):
         config['root'] = aws_out_dir
 
         self.default_runtime_attributes['queueArn'] = aws_batch_arn
+        self.default_runtime_attributes['scriptBucketName'] = aws_out_dir.replace('s3://', '')
 
 
 class CromwellBackendLocal(CromwellBackendBase):
