32ad0ef8fc60e2d276a7b8774868650b30a740a6
Jin Lee <leepc12@gmail.com>
2021-04-07 11:42:20 -0700

    bump ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='caper',
-    version='1.5.0',
+    version='1.5.1',
     python_requires='>=3.6',
     scripts=[
         'bin/caper',
