2d1b4922e7e20ddee1ec71706f61f5baffe8b0b2
Jin Lee <leepc12@gmail.com>
2021-04-07 11:50:17 -0700

    get version from __init__

--- setup.py
+++ setup.py
@@ -1,11 +1,43 @@
+import os
+import re
+from pathlib import Path
+
 import setuptools
+
+META_PATH = Path('caper', '__init__.py')
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with Path(HERE, *parts).open(encoding='utf-8') as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta), META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise
+
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='caper',
-    version='1.5.1',
+    version=find_meta('version'),
     python_requires='>=3.6',
     scripts=[
         'bin/caper',
