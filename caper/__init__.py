eaa0f68ba35a0069907f78e7f3c2a366a4bd3639
Jin wook Lee <leepc12@gmail.com>
2021-12-08 13:05:13 -0800

    bump ver

--- caper/__init__.py
+++ caper/__init__.py
@@ -2,4 +2,4 @@ from .caper_client import CaperClient, CaperClientSubmit
 from .caper_runner import CaperRunner
 
 __all__ = ['CaperClient', 'CaperClientSubmit', 'CaperRunner']
-__version__ = '2.1.1'
+__version__ = '2.1.2'
